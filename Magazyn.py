753057d67a465edb75e511295657d04165c5b8a3
Przemysław Jarocki <przemek.jarocki@gmail.com>
2022-03-02 11:55:24 +0100

    Szkielet

---
+++ Magazyn.py
@@ -0,0 +1,18 @@
+
+
+if __name__ == "__main__":
+
+    while exit == False:
+        pass
+    print ("Witaj w menadżerze Twojego Magazynu. Co chciałbyś zrobić?\nMENU: exit=wyjdż\t")
+    print("--------------\t-----\t---------\t----------------------")
+    print("Nazwa produktu\tIlość\tJednostka\tCena jednostkowa (PLN)")
+    print("--------------\t-----\t---------\t----------------------")
+    
+    
+    
+    
+    exit == input ()
+    if exit == exit:
+        print ("Zamykam... Do zobaczenia!")
+        exit ()
