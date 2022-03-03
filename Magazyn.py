17c9662f04e244e50b2ac1feab769ad3a74bce19
Przemysław Jarocki <przemek.jarocki@gmail.com>
2022-03-03 13:32:50 +0100

    Menu

--- Magazyn.py
+++ Magazyn.py
@@ -1,18 +1,18 @@
+items = [{"Name" : "Mleko","Quantity": "100","Unit": "litr","Unit_price": "2,40"},
+        {"Name" : "Cukier","Quantity": "150","Unit": "kg","Unit_price": "3,60"},
+        {"Name" : "Chleb","Quantity": "50","Unit": "szt","Unit_price": "4,50"}]
+
+
+
+def get_items (items):
+   
+    print("NAZWA:\t\t\tILOŚĆ:\t\t\tJEDN.:\t\t\tCENA(PLN):")
+    for i in items:
+        print (f"{i['Name']},\t\t\t {i['Quantity']},\t\t\t {i['Unit']},\t\t\t{i['Unit_price']}")
 
 
 if __name__ == "__main__":
 
-    while exit == False:
-        pass
-    print ("Witaj w menadżerze Twojego Magazynu. Co chciałbyś zrobić?\nMENU: exit=wyjdż\t")
-    print("--------------\t-----\t---------\t----------------------")
-    print("Nazwa produktu\tIlość\tJednostka\tCena jednostkowa (PLN)")
-    print("--------------\t-----\t---------\t----------------------")
-    
-    
-    
-    
-    exit == input ()
-    if exit == exit:
-        print ("Zamykam... Do zobaczenia!")
-        exit ()
+    print("---------------------------------------------------------")
+    print ("Witaj w menadżerze Twojego Magazynu. Co chciałbyś zrobić?\n\nMENU: |exit=wyjdż|\t|show=pokaż|")
+    print("---------------------------------------------------------")   
