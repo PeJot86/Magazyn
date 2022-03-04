1e206a5e6f992b70b450b64596519fa99492fd8f
Przemysław Jarocki <przemek.jarocki@gmail.com>
2022-03-04 14:12:32 +0100

    version with add option

--- Magazyn.py
+++ Magazyn.py
@@ -3,30 +3,37 @@ items = [{"Name" : "Mleko","Quantity": "100","Unit": "litr","Unit_price": "2,40"
         {"Name" : "Chleb","Quantity": "50","Unit": "szt","Unit_price": "4,50"}]
 
 def get_items (items):
-    print("NAZWA:\t\t\tILOŚĆ:\t\t\tJEDN.:\t\t\tCENA(PLN):")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+    print("NAZWA:\tILOŚĆ:\tJEDN.:\tCENA(PLN):")
     for i in items:
-        print (f"{i['Name']},\t\t\t {i['Quantity']},\t\t\t {i['Unit']},\t\t\t{i['Unit_price']}")
+        print (f"{i['Name']},\t{i['Quantity']},\t {i['Unit']},\t{i['Unit_price']}")
+   
 
-def add_items (Name, Quantity, Unit, Unit_price):
-    print ("Dodaję do magazynu")
+
+def add_items ():
+    dict = {"Name": var_name, "Quantity" : var_quantity, "Unit" : var_unit, "Unit_price" : var_price}
+    items.append(dict)
+    get_items (items)
+    print (f"\nDODAŁEM: {var_name}")
 
 if __name__ == "__main__":
 
-    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-    print ("Witaj w menadżerze Twojego Magazynu. Co chciałbyś zrobić?")
+    print ("\nWitaj w menadżerze Twojego Magazynu. Co chciałbyś zrobić?")
      
-
-
 while True:  
-    print("---------------------------------------------------------")
-    print ("MENU:\t|show=pokaż|\t|add=dodaj|\t|end=wyjdż|")
-    print("---------------------------------------------------------")
+    print("\n--------------------------------------------------------------------")
+    print ("MENU:\t|show=pokaż|\t|add=dodaj|\t|sub=usuń|\t|end=wyjdż|")
+    print("--------------------------------------------------------------------")
     resp = input("WYBIERZ:")
     if  resp == "show":
         get_items(items)
         continue
     elif resp == "add":
-            add_items()
+            var_name = input ("NAZWA ARTYKUŁU:")
+            var_quantity = input ("ILOŚĆ:")
+            var_unit = input ("JEDNOSTKA:")
+            var_price = input ("CENA JEDNOSTKOWA:")
+            add_items ()
             continue
     elif resp == "end":
             print ("Zamykam... Do zobaczenia! ;)")
