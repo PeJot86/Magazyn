d3d51bc85f9f740d5f995bbd5dc608306ee2f990
Przemysław Jarocki <przemek.jarocki@gmail.com>
2022-03-04 10:45:05 +0100

    Next version

--- Magazyn.py
+++ Magazyn.py
@@ -1,4 +1,4 @@
-items = [{"Name" : "Mleko w proszku","Quantity": "100","Unit": "litr","Unit_price": "2,40"},
+items = [{"Name" : "Mleko","Quantity": "100","Unit": "litr","Unit_price": "2,40"},
         {"Name" : "Cukier","Quantity": "150","Unit": "kg","Unit_price": "3,60"},
         {"Name" : "Chleb","Quantity": "50","Unit": "szt","Unit_price": "4,50"}]
 
@@ -7,22 +7,30 @@ def get_items (items):
     for i in items:
         print (f"{i['Name']},\t\t\t {i['Quantity']},\t\t\t {i['Unit']},\t\t\t{i['Unit_price']}")
 
+def add_items (Name, Quantity, Unit, Unit_price):
+    print ("Dodaję do magazynu")
 
 if __name__ == "__main__":
 
-    print("---------------------------------------------------------")
-    print ("Witaj w menadżerze Twojego Magazynu. Co chciałbyś zrobić?\n\nMENU: |exit=wyjdż|\t|show=pokaż|")
-    print("---------------------------------------------------------")   
-    
-while True:  
-    while (input() == "show"):
-        get_items(items)
-    
-    
-    (input("Zamknij=ENTER") == "end")
-    print ("Zamykam... Do zobaczenia!")
-    break
-   
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+    print ("Witaj w menadżerze Twojego Magazynu. Co chciałbyś zrobić?")
+     
 
-            
-        
+
+while True:  
+    print("---------------------------------------------------------")
+    print ("MENU:\t|show=pokaż|\t|add=dodaj|\t|end=wyjdż|")
+    print("---------------------------------------------------------")
+    resp = input("WYBIERZ:")
+    if  resp == "show":
+        get_items(items)
+        continue
+    elif resp == "add":
+            add_items()
+            continue
+    elif resp == "end":
+            print ("Zamykam... Do zobaczenia! ;)")
+            break
+    else:
+        print ("*Wybierz poprawną komendę z menu:*")
+        continue
