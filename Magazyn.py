94c11d9a48d6284f63704b70f67157dc818cca7c
Przemysław Jarocki <przemek.jarocki@gmail.com>
2022-03-09 11:36:51 +0100

    Next function

--- Magazyn.py
+++ Magazyn.py
@@ -1,39 +1,71 @@
-items = [{"Name" : "Mleko","Quantity": "100","Unit": "litr","Unit_price": "2,40"},
-        {"Name" : "Cukier","Quantity": "150","Unit": "kg","Unit_price": "3,60"},
-        {"Name" : "Chleb","Quantity": "50","Unit": "szt","Unit_price": "4,50"}]
+items = [{"Name" : "mleko","Quantity": 100,"Unit": "litr","Unit_price": 2.40},
+        {"Name" : "cukier","Quantity": 150,"Unit": "kg","Unit_price": 3.60},
+        {"Name" : "chleb","Quantity": 50,"Unit": "szt","Unit_price": 4.50}]
 
-def get_items (items):
+sort_items = sorted(items, key=lambda x: (x['Name']))
+sum_list=[]
+sum_price = sum(sum_list)
+div = 0
+for i in items:     
+    div = i["Quantity"] * i["Unit_price"]
+    sum_list.append (div)
+    sum_price = sum(sum_list)
+
+def get_items (sort_items):
+    sort_items = sorted(items, key=lambda x: (x['Name']))
     print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-    print("NAZWA:\tILOŚĆ:\tJEDN.:\tCENA(PLN):")
-    for i in items:
+    print("NAZWA:\tILOŚĆ:\tJEDN.:\tCENA JEDN.(PLN):")
+    for i in sort_items:
         print (f"{i['Name']},\t{i['Quantity']},\t {i['Unit']},\t{i['Unit_price']}")
-   
 
-
-def add_items ():
-    dict = {"Name": var_name, "Quantity" : var_quantity, "Unit" : var_unit, "Unit_price" : var_price}
+def add_items (name, quantity, unit, price):       
+    dict = {"Name": name, "Quantity" : quantity, "Unit" : unit, "Unit_price" : price}
     items.append(dict)
-    get_items (items)
-    print (f"\nDODAŁEM: {var_name}")
+    get_items (sort_items)
+    print (f"\nDODAŁEM: {name}")
+
+def sell_items (sell_name, sell_quantity):
+    quant_numb = 0
+    for i in items:
+        if  i['Name'] == sell_name:
+            quant_numb = i['Quantity']
+            sell_quant_sum = quant_numb - sell_quantity
+            i['Quantity'] = sell_quant_sum
+            print (f"Sprzedaję {sell_quantity} {sell_name}, pozostało {sell_quant_sum}")
 
 if __name__ == "__main__":
-
-    print ("\nWitaj w menadżerze Twojego Magazynu. Co chciałbyś zrobić?")
-     
+    print ("\nWitaj w menadżerze Twojego Magazynu. Co chciałbyś zrobić?")     
 while True:  
     print("\n--------------------------------------------------------------------")
-    print ("MENU:\t|show=pokaż|\t|add=dodaj|\t|sub=usuń|\t|end=wyjdż|")
+    print ("MENU:\t|show=pokaż|\t|add=dodaj|\t|sell=sprzedaj|\t\t|end=wyjdż|")
     print("--------------------------------------------------------------------")
-    resp = input("WYBIERZ:")
+    resp = input("WYBIERZ: ")
     if  resp == "show":
-        get_items(items)
+        get_items(sort_items)
+        print (f"\t\t\tRAZEM (PLN) {sum_price}")
         continue
     elif resp == "add":
-            var_name = input ("NAZWA ARTYKUŁU:")
-            var_quantity = input ("ILOŚĆ:")
-            var_unit = input ("JEDNOSTKA:")
-            var_price = input ("CENA JEDNOSTKOWA:")
-            add_items ()
+            name = input ("NAZWA ARTYKUŁU: ")
+            quantity = float(input ("ILOŚĆ: "))
+            unit = input ("JEDNOSTKA: ")
+            price = float(input ("CENA: "))
+            add_items (name, quantity, unit, price)
+            for i in items:
+                div = i["Quantity"] * i["Unit_price"]
+                sum_list.append (div)
+                sum_price = sum(sum_list)
+            continue
+    elif  resp == "sell":
+            sell_name = input ("Co sprzedajemy?: ")
+            for i in items:
+                if  i['Name'] == sell_name:
+                    print ("Jest!")
+                    break
+            else:   
+                print ("Nie posiadamy takiego towaru!")
+                continue                       
+            sell_quantity = float(input("Podaj ilość: "))
+            sell_items (sell_name, sell_quantity)                                          
             continue
     elif resp == "end":
             print ("Zamykam... Do zobaczenia! ;)")
